316cca24b8ab085c9d7841ba1716c0ab65fbdf38
wangyu <123456>
2018-01-22 11:07:25 +0800

    aa

--- nio/src/main/java/com/channel/socket/SocketDemo.java
+++ nio/src/main/java/com/channel/socket/SocketDemo.java
@@ -2,15 +2,36 @@ package com.channel.socket;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
+
+import static java.lang.Thread.sleep;
 
 public class SocketDemo {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        connect2();
+    }
+
+    public static void connect1() throws IOException {
         Socket socket = new Socket("localhost", 1234);
         byte[] bytes = new byte[100];
         InputStream inputStream = socket.getInputStream();
         inputStream.read(bytes);
         System.out.println(new String(bytes));
+    }
+
+    public static void connect2() throws Exception {
+        InetSocketAddress addr = new InetSocketAddress("localhost", 1234);
+        SocketChannel sc = SocketChannel.open();
+//        Socket sk = sc.socket();
+        sc.configureBlocking(false);
+        sc.connect(addr);
+        while(!sc.finishConnect()) {
+            System.out.println("connect...");
+        }
+        System.out.println("connect finnish");
+        sc.close();
     }
 }
